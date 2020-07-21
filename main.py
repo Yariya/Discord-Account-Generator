e91b8de67adbbb4f3216b34bf9ced3a3794ce68d
rexjohannes <53578137+rexjohannes@users.noreply.github.com>
2020-07-21 15:51:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ import asyncio
 import os
 
 from discord.ext import commands
-from configuration import config
 
-__name__ = 'botname'
-__version__ = 0.1
 
-bot = commands.Bot(command_prefix=config.__prefix__)
+bot = commands.Bot(command_prefix="!")
 bot.remove_command('help')
 
 def get_total_accounts():
@@ -23,7 +20,7 @@ def get_total_accounts():
         spotify_count_lines = len(spotify.readlines())
 
     with open('./crunchyroll.txt', 'r') as origin:
-        crunchyroll_count_lines = len(crunchyroll.readlines())
+        origin_count_lines = len(origin.readlines())
 
 
     with open('./hulu.txt', 'r') as hulu:
@@ -31,21 +28,14 @@ def get_total_accounts():
 
 
 
-    return minecraft_count_lines + nordvpn_count_lines + spotify_count_lines + crunchyroll_count_lines + hulu_count_lines
+    return minecraft_count_lines + nordvpn_count_lines + spotify_count_lines + origin_count_lines + hulu_count_lines
 
-async def auto_reload():
-    await bot.wait_until_ready()
-    while not bot.is_closed():
-        await bot.change_presence(status=discord.Status, activity=discord.Activity(name=f'{get_total_accounts()} accounts.', type=discord.ActivityType.watching))
-        await asyncio.sleep(10 * 60)
 
 @bot.event
 async def on_ready():
-    for filename in os.listdir('./extensions'):
-        if filename.endswith('.py'):
-            bot.load_extension(f'extensions.{filename[:-3]}')
+    await bot.change_presence(status=discord.Status, activity=discord.Activity(name=f'{get_total_accounts()} accounts.', type=discord.ActivityType.watching))
+    bot.load_extension("commands")
 
     print(f'Successfully logged in as {bot.user.name}!')
 
-bot.loop.create_task(auto_reload())
-bot.run(config.__token__)
+bot.run("token")
